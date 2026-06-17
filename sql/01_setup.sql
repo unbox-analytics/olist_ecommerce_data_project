7cfe7686b7483660af3e595789a15f7737cf2be7
Malachi Lopes Mendes <malachi.mendes@gmail.com>
2026-06-17 13:08:55 +0530

    commit

--- sql/01_setup.sql
+++ sql/01_setup.sql
@@ -8,112 +8,178 @@ USE olist_data;
     CREATE SCHEMA error; -- data with error flags
     CREATE SCHEMA audit; -- audit log
 -- create audit table to hold information about each run
+    drop table if exists audit.etl_runs; 
     create table audit.etl_runs(
         run_id int identity(1,1),
         table_name varchar(50),
         rows_loaded int,
         rows_rejected int,
-        load_datetime datetime default getdate()
+        load_datetime datetime default getdate(),
+        status VARCHAR(20),
+        error_message VARCHAR(4000)
     );
--- creating staging tables without data
+
+-- creating staging tables and inserting data from raw
     -- product table
-        select product_id,
-                product_category_name,
-                product_name_lenght as product_name_length, -- correct typo
-                product_description_lenght as product_description_length,  -- correct typo
-                product_photos_qty, 
-                product_weight_g,
-                product_length_cm,
-                product_height_cm,
-                product_width_cm
-            into stg.products 
-            from raw.products
-            where 1=0
-    
+        drop table if exists stg.products;
+        CREATE TABLE stg.products
+        (
+        product_id VARCHAR(32) NOT NULL,
+        product_category_name VARCHAR(50),
+        product_name_length INT,
+        product_description_length INT,
+        product_photos_qty INT,
+        product_weight_g INT,
+        product_length_cm INT,
+        product_height_cm INT,
+        product_width_cm INT,
+
+        load_datetime DATETIME NOT NULL DEFAULT GETDATE(),
+        source_file VARCHAR(255),
+        batch_id INT
+        );
+        drop table if exists clean.products;
+        CREATE TABLE clean.products
+        (
+            product_id VARCHAR(32) NOT NULL,
+            product_category_name VARCHAR(50),
+            product_name_length INT,
+            product_description_length INT,
+            product_photos_qty INT,
+            product_weight_g INT,
+            product_length_cm INT,
+            product_height_cm INT,
+            product_width_cm INT,
+
+            load_datetime DATETIME NOT NULL,
+            batch_id INT,
+
+            CONSTRAINT PK_clean_products
+                PRIMARY KEY (product_id)
+        );
+        
+        drop table if exists error.products;
+        CREATE TABLE error.products
+        (
+            product_id VARCHAR(32),
+            product_category_name VARCHAR(50),
+            product_name_length INT,
+            product_description_length INT,
+            product_photos_qty INT,
+            product_weight_g INT,
+            product_length_cm INT,
+            product_height_cm INT,
+            product_width_cm INT,
+
+            error_flags VARCHAR(500),
+
+            load_datetime DATETIME NOT NULL,
+            source_file VARCHAR(255),
+            batch_id INT
+        );
+
+
+
     -- customer table
         drop table if exists stg.customers;
-        select *, row_number() over (partition by customer_id order by customer_id) as row_num into stg.customers from raw.customers
-        where 1=0;
+        CREATE TABLE stg.customers
+        (
+            customer_id VARCHAR(32) NOT NULL,
+            customer_unique_id VARCHAR(32) NOT NULL,
+            customer_zip_code_prefix CHAR(5),
+            customer_city VARCHAR(100),
+            customer_state CHAR(2),
 
-        insert into stg.customers
-            select  customer_id, 
-                customer_unique_id, 
-                customer_zip_code_prefix, 
-                customer_city, 
-                customer_state, 
-                row_number() over (partition by customer_id order by customer_id) as row_num
-            from raw.customers
+            load_datetime DATETIME NOT NULL DEFAULT GETDATE(),
+            source_file VARCHAR(255),
+            batch_id INT
+        );
 
     -- orders table
+        drop table if exists stg.orders;
+        CREATE TABLE stg.orders
+        (
+        order_id VARCHAR(32) NOT NULL,
+        customer_id VARCHAR(32) NOT NULL,
+        order_status VARCHAR(20),
+        order_purchase_timestamp DATETIME,
+        order_approved_at DATETIME,
+        order_delivered_carrier_date DATETIME,
+        order_delivered_customer_date DATETIME,
+        order_estimated_delivery_date DATETIME,
+
+        load_datetime DATETIME NOT NULL DEFAULT GETDATE(),
+        source_file VARCHAR(255),
+        batch_id INT
+        );
+
+
     -- order_items table
     -- order_reviews table
     -- order_payments table
     -- sellers table
-
-
-
     -- creating error table without data
 
-    select * 
-    into error.products 
-    from stg.products
-    where 1=0
-    -- add columns to record errors
-    alter table error.products
-    add 
-    flag_error bit default 0,
-    error_description varchar(100)
+        select * 
+        into error.products 
+        from stg.products
+        where 1=0
+        -- add columns to record errors
+        alter table error.products
+        add 
+        flag_error bit default 0,
+        error_description varchar(100)
 
-    -- creating clean table without data
-    drop table if exists clean.products; 
+        -- creating clean table without data
+        drop table if exists clean.products; 
 
-    select * 
-    into clean.products 
-    from stg.products
-    where 1=0
+        select * 
+        into clean.products 
+        from stg.products
+        where 1=0
 
-    -- loading raw data into staging table
-    insert into stg.products
-    select * from raw.products 
+        -- loading raw data into staging table
+        insert into stg.products
+        select * from raw.products 
 
--- Data Quality checks
--- product_category_name should not be null
-insert into error.products(
-    product_id, 
-    product_category_name, 
-    product_name_length, 
-    product_description_length, 
-    product_photos_qty, 
-    product_weight_g, 
-    product_length_cm, 
-    product_height_cm, 
-    product_width_cm, 
-    flag_error,
-    error_description
-    )
-select     product_id, 
-    product_category_name, 
-    product_name_length, 
-    product_description_length, 
-    product_photos_qty, 
-    product_weight_g, 
-    product_length_cm, 
-    product_height_cm, 
-    product_width_cm, 
-    1, 
-    'Product category name is null' 
-from stg.products
-where product_category_name is null 
+    -- Data Quality checks
+    -- product_category_name should not be null
+        insert into error.products(
+            product_id, 
+            product_category_name, 
+            product_name_length, 
+            product_description_length, 
+            product_photos_qty, 
+            product_weight_g, 
+            product_length_cm, 
+            product_height_cm, 
+            product_width_cm, 
+            flag_error,
+            error_description
+            )
+        select     product_id, 
+            product_category_name, 
+            product_name_length, 
+            product_description_length, 
+            product_photos_qty, 
+            product_weight_g, 
+            product_length_cm, 
+            product_height_cm, 
+            product_width_cm, 
+            1, 
+            'Product category name is null' 
+        from stg.products
+        where product_category_name is null 
 
-select * from error.products
+        select * from error.products
 
 
--- data quality checks
--- product id should not be null
--- product category name should exist in the category table. if null it should be renamed to undefined
--- product category name in english should be pulled into the staging table, brazillian name dropped
--- if product category name is > 0 then len product name should be used instead
--- photos quantity null shpuld be flagged
--- no description should be flagged
--- weight length height width should be non null
+        -- data quality checks
+        -- product id should not be null
+        -- product category name should exist in the category table. if null it should be renamed to undefined
+        -- product category name in english should be pulled into the staging table, brazillian name dropped
+        -- if product category name is > 0 then len product name should be used instead
+        -- photos quantity null shpuld be flagged
+        -- no description should be flagged
+        -- weight length height width should be non null
 
---
+++ sql/02_etl_procedures.sql
@@ -0,0 +1,167 @@
+
+    CREATE OR ALTER PROCEDURE stg.usp_load_products
+    AS
+    BEGIN
+        SET NOCOUNT ON;
+            DECLARE @rows_loaded INT = 0, @rows_rejected INT = 0;
+            BEGIN TRY
+                BEGIN TRANSACTION
+                                TRUNCATE TABLE stg.products;
+                TRUNCATE TABLE error.products;
+            TRUNCATE TABLE clean.products;
+                -- creates staging customers table
+
+                DECLARE @batch_id INT = (SELECT ISNULL(MAX(run_id), 0) + 1 FROM audit.etl_runs);
+                INSERT INTO stg.products
+                (
+                    product_id,
+                    product_category_name,
+                    product_name_length,
+                    product_description_length,
+                    product_photos_qty,
+                    product_weight_g,
+                    product_length_cm,
+                    product_height_cm,
+                    product_width_cm,
+                    load_datetime,
+                    source_file,
+                    batch_id
+                )
+                SELECT
+                    product_id,
+                    product_category_name,
+                    product_name_lenght as product_name_length,
+                    product_description_lenght as product_description_length,
+                    product_photos_qty,
+                    product_weight_g,
+                    product_length_cm,
+                    product_height_cm,
+                    product_width_cm,
+                    getdate() as load_datetime,
+                    'product.csv' as source_file,
+                    @batch_id
+                FROM raw.products;
+
+                -- creates error table 
+
+                ;WITH V as (
+                    SELECT * , 
+                    ROW_NUMBER() OVER (PARTITION BY product_id ORDER BY (select null)) AS row_num 
+                FROM stg.products
+                ),
+                validations as (
+                    select 
+                    product_id,
+                    product_category_name,
+                    product_name_length,
+                    product_description_length,
+                    product_photos_qty,
+                    product_weight_g,
+                    product_length_cm,
+                    product_height_cm,
+                    product_width_cm,
+                    Nullif(
+                        concat(
+                case when product_category_name is null then 'Product category name is null; ' end,
+                case when product_id is null then 'Product id is null; ' end,
+                case when product_weight_g < 0 then 'Product weight is negative; ' end,
+                case when product_length_cm < 0 then 'Product length is negative; ' end,
+                case when product_height_cm < 0 then 'Product height is negative; ' end,
+                case when row_num > 1 then 'Duplicate product; ' end,
+                case when product_width_cm < 0 then 'Product width is negative; ' end),
+                '')as error_flags,
+                    load_datetime,
+                    source_file,
+                    batch_id
+                from v
+                )insert into #validations
+                select * from validations
+
+                INSERT INTO error.products(
+                    product_id,
+                    product_category_name,
+                    product_name_length,
+                    product_description_length,
+                    product_photos_qty,
+                    product_weight_g,
+                    product_length_cm,
+                    product_height_cm,
+                    product_width_cm,
+
+                    error_flags,
+
+                    load_datetime,
+                    source_file,
+                    batch_id
+                )
+                SELECT 
+                    product_id,
+                    product_category_name,
+                    product_name_length,
+                    product_description_length,
+                    product_photos_qty,
+                    product_weight_g,
+                    product_length_cm,
+                    product_height_cm,
+                    product_width_cm,
+                    error_flags,
+                    load_datetime,
+                    source_file,
+                    batch_id
+                    FROM #validations
+                    WHERE error_flags IS NOT NULL;
+            SET @rows_rejected = @@ROWCOUNT;
+
+                    INSERT INTO clean.products (
+                        product_id, product_category_name,
+                        product_name_length,          -- fixed typo
+                        product_description_length,   -- fixed typo
+                        product_photos_qty,
+                        product_weight_g, product_length_cm, product_height_cm, product_width_cm,
+                        load_datetime,
+                        batch_id
+                    )
+                    SELECT
+                        product_id, product_category_name,
+                        product_name_length, product_description_length, product_photos_qty,
+                        product_weight_g, product_length_cm, product_height_cm, product_width_cm,load_datetime,
+                        batch_id
+                    FROM #validations
+                    WHERE error_flags IS NULL;
+                    SET @rows_loaded = @@ROWCOUNT;
+                COMMIT TRANSACTION;
+
+                    -- Audit success
+                    INSERT INTO audit.etl_runs
+                    (table_name, rows_loaded, rows_rejected, status, error_message)
+                    VALUES
+                    (
+                        'products', @rows_loaded, @rows_rejected, 'SUCCESS', NULL
+                    );
+            END TRY
+
+            BEGIN CATCH
+
+                IF @@TRANCOUNT > 0
+                    ROLLBACK TRANSACTION;
+
+                INSERT INTO audit.etl_runs
+                (
+                    table_name,
+                    rows_loaded,
+                    rows_rejected,
+                    status,
+                    error_message
+                )
+                VALUES
+                (
+                    'products',
+                    0,
+                    0,
+                    'FAILED',
+                    ERROR_MESSAGE()
+                );
+
+                THROW;
+            END CATCH
+    END;
